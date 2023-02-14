210245c9fd85d837dc21dd70ff3d62ef301bbf0c
Aasif Alvi <asif.alvi@epicglobal.tech>
2023-02-14 18:33:06 +0530

    Changes

--- controllers/sale-controller.ts
+++ controllers/sale-controller.ts
@@ -2,247 +2,283 @@ import { Request, Response } from "express";
 import { isValidObjectId } from "mongoose";
 import row from "../models/row";
 import sale from "../models/sale";
-import { deleteSaleService, generateInvoiceNumber, getAllSaleService, getRowService, getSaleByIdService } from "../services/sale-service";
+import {
+  deleteSaleService,
+  generateInvoiceNumber,
+  getAllSaleService,
+  getRowService,
+  getSaleByIdService,
+} from "../services/sale-service";
 
 // sale controller
 export const handleSaleController = async (req: Request, res: Response) => {
-    try {
-        const { name, number, itemName, qty, unit, priceUnitTax, saletax } = req.body;
-        if (!name || !number || !itemName || !unit) {
-            return res.status(500).send({ success: false, message: "All fields are required" });
-        }
-
-        // saletax.taxableAmount = ((saletax?.taxableAmount * saletax?.tax) / 100) + 100;
-        const taxOnAmount = ((saletax?.taxOnAmount * saletax?.tax) / 100) + 100;
-
-        // generating invoice number... 
-        //const invoiceNumber = generateInvoiceNumber();
-        const findDuplicateField = await sale.findOne({
-            number
-        });
-
-        if (findDuplicateField) {
-            return res.status(400).send({
-                success: false,
-                message: "Number already exists & must be unique",
-            });
-        }
-        let date = new Date();
-        const newDate = date.toString();
-        const sales = new sale({
-            name, number,
-            invoiceDate: newDate,
-            amount: taxOnAmount,
-            itemName, qty, unit, priceUnitTax, saletax,
-        });
-
-        sales.save((err, data) => {
-            if (err) throw err;
-            res.send({ success: true, message: "Sale Added Successful", data });
-        });
-    } catch (error: any) {
-        res.status(500).send(error?.message);
+  try {
+    const {
+      name,
+      number,
+      itemName,
+      address,
+      qty,
+      unit,
+      priceUnitTax,
+      saletax,
+      paymentMode,
+      description,
+      amount,
+    } = req.body;
+    if (!name || !number || !itemName || !unit || !address) {
+      return res
+        .status(500)
+        .send({ success: false, message: "All fields are required" });
     }
-}
+
+    // saletax.taxableAmount = ((saletax?.taxableAmount * saletax?.tax) / 100) + 100;
+    const taxOnAmount = (saletax?.taxOnAmount * saletax?.tax) / 100 + 100;
+
+    // generating invoice number...
+    //const invoiceNumber = generateInvoiceNumber();
+    const findDuplicateField = await sale.findOne({
+      number,
+    });
+
+    if (findDuplicateField) {
+      return res.status(400).send({
+        success: false,
+        message: "Number already exists & must be unique",
+      });
+    }
+    let date = new Date();
+    const newDate = date.toString();
+    const sales = new sale({
+      name,
+      number,
+      invoiceDate: newDate,
+      amount: taxOnAmount,
+      itemName,
+      address,
+      qty,
+      unit,
+      paymentMode,
+      description,
+      priceUnitTax,
+      saletax,
+    });
+
+    sales.save((err, data) => {
+      if (err) throw err;
+      res.send({ success: true, message: "Sale Added Successful", data });
+    });
+  } catch (error: any) {
+    res.status(500).send(error?.message);
+  }
+};
 
 // get all sale controller
 export const handleAllSaleController = async (req: Request, res: Response) => {
-    try {
-        const Sale = await getAllSaleService();
-        res.status(200).send({ success: true, message: Sale });
-    } catch (error: any) {
-        res.status(400).send({ error: error.message });
-    }
-}
+  try {
+    const Sale = await getAllSaleService();
+    res.status(200).send({ success: true, message: Sale });
+  } catch (error: any) {
+    res.status(400).send({ error: error.message });
+  }
+};
 
 // get sale by id controller
 export const handleSaleByIdController = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        if (!id || !isValidObjectId(id)) {
-            return res
-                .status(400)
-                .send({ success: false, message: "Invalid Id provided." });
-        }
-
-        const response = await getSaleByIdService(req.params.id);
-        res.status(200).send({ response });
-    } catch (error: any) {
-        res.status(400).send({ error: error.message });
-
+  try {
+    const { id } = req.params;
+    if (!id || !isValidObjectId(id)) {
+      return res
+        .status(400)
+        .send({ success: false, message: "Invalid Id provided." });
     }
-}
+
+    const response = await getSaleByIdService(req.params.id);
+    res.status(200).send({ response });
+  } catch (error: any) {
+    res.status(400).send({ error: error.message });
+  }
+};
 
 // delete sale controller
 export const deleteSaleController = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        if (!id || !isValidObjectId(id)) {
-            return res
-                .status(400)
-                .send({ success: false, message: "ID is required." });
-        }
-
-        const deleteSale = await deleteSaleService(id);
-        res.status(200).send({ success: true, message: "Sale deleted successful" })
-    } catch (error: any) {
-        res.status(400).send({ error: error.message });
+  try {
+    const { id } = req.params;
+    if (!id || !isValidObjectId(id)) {
+      return res
+        .status(400)
+        .send({ success: false, message: "ID is required." });
     }
-}
+
+    const deleteSale = await deleteSaleService(id);
+    res.status(200).send({ success: true, message: "Sale deleted successful" });
+  } catch (error: any) {
+    res.status(400).send({ error: error.message });
+  }
+};
 
 // update sale controller
 export const updateSaleController = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const { name, number, itemName, qty, unit, saletax, priceUnitTax } = req.body;
+  try {
+    const { id } = req.params;
+    const { name, number, itemName, qty, unit, saletax, priceUnitTax } =
+      req.body;
 
-        // saletax.taxableAmount = ((saletax?.taxableAmount * saletax?.tax) / 100) + 100;
-        const taxOnAmount = ((saletax?.taxOnAmount * saletax?.tax) / 100) + 100;
+    // saletax.taxableAmount = ((saletax?.taxableAmount * saletax?.tax) / 100) + 100;
+    const taxOnAmount = (saletax?.taxOnAmount * saletax?.tax) / 100 + 100;
 
-        if (!id || !isValidObjectId(id)) {
-            return res.status(400).send({
-                success: false,
-                message: "Valid id is required",
-            });
-        }
-        // updating 
-        await sale
-            .findOneAndUpdate(
-                { _id: id },
-                {
-                    $set: {
-                        name,
-                        number,
-                        itemName,
-                        qty,
-                        unit,
-                        amount: taxOnAmount,
-                        saletax,
-                        priceUnitTax,
-                    },
-                },
-            )
-            .then((data) => {
-                res.send(data);
-            })
-            .catch(async (err) => {
-                res.status(500).send(err);
-            });
-    } catch (error: any) {
-        res.status(500).send(error);
+    if (!id || !isValidObjectId(id)) {
+      return res.status(400).send({
+        success: false,
+        message: "Valid id is required",
+      });
     }
-}
+    // updating
+    await sale
+      .findOneAndUpdate(
+        { _id: id },
+        {
+          $set: {
+            name,
+            number,
+            itemName,
+            qty,
+            unit,
+            amount: taxOnAmount,
+            saletax,
+            priceUnitTax,
+          },
+        }
+      )
+      .then((data) => {
+        res.send(data);
+      })
+      .catch(async (err) => {
+        res.status(500).send(err);
+      });
+  } catch (error: any) {
+    res.status(500).send(error);
+  }
+};
 
 // add sale row controller
 export const addSaleRowController = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const { itemName, saletax, qty, unit, priceUnitTax } = req.body;
+  try {
+    const { id } = req.params;
+    const { itemName, saletax, qty, unit, priceUnitTax } = req.body;
 
-        // saletax.taxableAmount = ((saletax?.taxableAmount * saletax?.tax) / 100) + 100;
-        // console.log(saletax.taxableAmount);
-        const taxOnAmount = ((saletax?.taxOnAmount * saletax?.tax) / 100) + 100;
-    
-        if (!itemName) {
-            return res.status(400).json({ message: "Item name cannot be empty." });
-        }
+    // saletax.taxableAmount = ((saletax?.taxableAmount * saletax?.tax) / 100) + 100;
+    // console.log(saletax.taxableAmount);
+    const taxOnAmount = (saletax?.taxOnAmount * saletax?.tax) / 100 + 100;
 
-        if (!id || !isValidObjectId(id)) {
-            return res.status(400).json({ message: "Invalid ID provided." });
-        }
-
-        const saleFind = await sale.findOne({ _id: id }).populate("row").lean();
-        if (!saleFind) {
-            return res.status(400).json({ message: "Invalid ID provided." });
-        }
-
-        const addrow = new row({
-            amount: taxOnAmount,
-            itemName: itemName,
-            qty: qty,
-            unit: unit,
-            priceUnitTax,
-            saletax,
-        });
-
-        addrow.save(req.body, async (err: any, data: any) => {
-            if (err) {
-                return res.status(500).send(err);
-            }
-            await sale.findByIdAndUpdate(id, {
-                $push: {
-                    row: data._id,
-                },
-            });
-            res.status(200).json({
-                message: "Row Added Successfully",
-                data,
-            });
-        });
-    } catch (error: any) {
-        res.status(400).send({ error: error.message });
+    if (!itemName) {
+      return res.status(400).json({ message: "Item name cannot be empty." });
     }
-}
 
-// get sale row conrtoller 
+    if (!id || !isValidObjectId(id)) {
+      return res.status(400).json({ message: "Invalid ID provided." });
+    }
+
+    const saleFind = await sale.findOne({ _id: id }).populate("row").lean();
+    if (!saleFind) {
+      return res.status(400).json({ message: "Invalid ID provided." });
+    }
+
+    const addrow = new row({
+      amount: taxOnAmount,
+      itemName: itemName,
+      qty: qty,
+      unit: unit,
+      priceUnitTax,
+      saletax,
+    });
+
+    addrow.save(req.body, async (err: any, data: any) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      await sale.findByIdAndUpdate(id, {
+        $push: {
+          row: data._id,
+        },
+      });
+      res.status(200).json({
+        message: "Row Added Successfully",
+        data,
+      });
+    });
+  } catch (error: any) {
+    res.status(400).send({ error: error.message });
+  }
+};
+
+// get sale row conrtoller
 export const getRowController = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        if (!id || !isValidObjectId(id)) {
-            return res.status(400).json({ message: "Invalid ID provided." });
-        }
-
-        const getRow = await getRowService(req.params.id);
-        res.status(200).send({ success: true, data: getRow });
-    } catch (error: any) {
-        res.status(500).send({ error: error.message });
+    if (!id || !isValidObjectId(id)) {
+      return res.status(400).json({ message: "Invalid ID provided." });
     }
-}
+
+    const getRow = await getRowService(req.params.id);
+    res.status(200).send({ success: true, data: getRow });
+  } catch (error: any) {
+    res.status(500).send({ error: error.message });
+  }
+};
 
 // update row controller
 export const updateRowController = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const { itemName, qty, unit, saletax, priceUnitTax } = req.body;
+  try {
+    const { id } = req.params;
+    const { itemName, qty, unit, saletax, priceUnitTax } = req.body;
 
-        // update tax on amount..
-        const taxOnAmount = ((saletax?.taxOnAmount * saletax?.tax) / 100) + 100;
+    // update tax on amount..
+    const taxOnAmount = (saletax?.taxOnAmount * saletax?.tax) / 100 + 100;
 
-        if (!id || !isValidObjectId(id)) {
-            return res.status(400).send({
-                success: false,
-                message: "Valid id is required",
-            });
-        }
-        // updating row
-        await row.findOneAndUpdate(
-            { _id: id },
-            {
-                $set: {
-                    itemName, qty, unit, saletax, priceUnitTax, amount: taxOnAmount,
-                },
-            },
-        ).then((data) => {
-            res.send(data);
-        }).catch(async (err) => {
-            res.status(500).send(err);
-        });
-    } catch (error: any) {
-        res.status(500).send({ error: error.message });
+    if (!id || !isValidObjectId(id)) {
+      return res.status(400).send({
+        success: false,
+        message: "Valid id is required",
+      });
     }
-}
+    // updating row
+    await row
+      .findOneAndUpdate(
+        { _id: id },
+        {
+          $set: {
+            itemName,
+            qty,
+            unit,
+            saletax,
+            priceUnitTax,
+            amount: taxOnAmount,
+          },
+        }
+      )
+      .then((data) => {
+        res.send(data);
+      })
+      .catch(async (err) => {
+        res.status(500).send(err);
+      });
+  } catch (error: any) {
+    res.status(500).send({ error: error.message });
+  }
+};
 
 // delete sale row controller
 export const deleteSaleRowController = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    try {
-        const deletedBed = await row.findByIdAndDelete(id);
-        res.json({
-            message: "Sale Row Deleted Succesfully",
-            data: deletedBed,
-        });
-    } catch (error: any) {
-        res.status(500).send(error);
-    }
-}
+  const { id } = req.params;
+  try {
+    const deletedBed = await row.findByIdAndDelete(id);
+    res.json({
+      message: "Sale Row Deleted Succesfully",
+      data: deletedBed,
+    });
+  } catch (error: any) {
+    res.status(500).send(error);
+  }
+};
--- models/sale.ts
+++ models/sale.ts
@@ -3,99 +3,113 @@ import row from "../models/row";
 import saleCounter from "./saleCounters";
 
 export interface Sale extends Document {
-    invoiceDate: Date,
-    name: string;
-    number: string;
-    amount: number;
-    itemName: string;
-    qty: number;
-    unit: string;
-    priceUnitTax: string;
-    saletax: {
-        tax: number;
-        taxOnAmount: number;
-    }
-    row: string[];
-    saleId: number;
-
+  invoiceDate: Date;
+  name: string;
+  number: string;
+  amount: number;
+  itemName: string;
+  address: string;
+  qty: number;
+  unit: string;
+  priceUnitTax: string;
+  saletax: {
+    tax: number;
+    taxOnAmount: number;
+  };
+  row: string[];
+  saleId: number;
+  paymentMode: string;
+  description: string;
 }
 
 const saleSchema = new Schema<Sale>(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        number: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        itemName: {
-            type: String,
-            required: true
-        },
-        invoiceDate: {
-            type: Date,
-            default: Date.now,
-        },
-        amount: {
-            type: Number,
-            required: false,
-        },
-        unit: {
-            type: String,
-            required: true,
-        },
-        qty: {
-            type: Number,
-            required: true,
-            default: 0,
-        },
-        priceUnitTax: {
-            type: String,
-            required: false,
-            default: "Without Tax",
-        },
-        saleId: {
-            type: Number,
-            required: false,
-        },
-        saletax: {
-            tax: {
-                type: Number,
-                required: false,
-            },
-            taxOnAmount: {
-                type: Number,
-                required: false,
-            },
-        },
-        row: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: row,
-            }
-        ]
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    number: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    itemName: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    invoiceDate: {
+      type: Date,
+      default: Date.now,
+    },
+    amount: {
+      type: Number,
+      required: false,
+    },
+    unit: {
+      type: String,
+      required: true,
+    },
+    qty: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    priceUnitTax: {
+      type: String,
+      required: false,
+      default: "Without Tax",
+    },
+    saleId: {
+      type: Number,
+      required: false,
+    },
+    paymentMode: {
+      type: String,
+      required: false,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    saletax: {
+      tax: {
+        type: Number,
+        required: false,
+      },
+      taxOnAmount: {
+        type: Number,
+        required: false,
+      },
+    },
+    row: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: row,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
 saleSchema.pre("save", function (next) {
-    saleCounter.findByIdAndUpdate(
-        { _id: "saleId" },
-        { $inc: { saleInvoiceSeq: 1 } },
-        { new: true, upsert: true },
-        (err, counter) => {
-            if (err) {
-                return next(err);
-            }
-            this.saleId = counter.saleInvoiceSeq;
-            next();
-        }
-    );
+  saleCounter.findByIdAndUpdate(
+    { _id: "saleId" },
+    { $inc: { saleInvoiceSeq: 1 } },
+    { new: true, upsert: true },
+    (err, counter) => {
+      if (err) {
+        return next(err);
+      }
+      this.saleId = counter.saleInvoiceSeq;
+      next();
+    }
+  );
 });
 
 export default model<Sale>("sale", saleSchema);
